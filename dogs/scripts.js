19557a3b09841580fc23b7abea47de7141685369
ValkyrieKiwi <armstrong.d30320@gmail.com>
2022-07-08 14:41:48 +0100

    commit

--- dogs/scripts.js
+++ dogs/scripts.js
@@ -10,7 +10,7 @@ app.appendChild(logo)
 app.appendChild(container)
 
 var request = new XMLHttpRequest()
-request.open('GET', 'https://dog.ceo/api/breed/Germanshepherd/images', true)
+request.open('GET', 'https://dog.ceo/api/breed/spaniel/cocker/images', true)
 request.onload = function () {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response)
@@ -19,8 +19,8 @@ request.onload = function () {
       const card = document.createElement('div')
       card.setAttribute('class', 'card')
 
-      const h1 = document.createElement('h1')
-      h1.textContent = movie.title
+      //const h1 = document.createElement('h1')
+      //h1.textContent = movie.title
 
       const p = document.createElement('p')
       movie.description = movie.description.substring(0, 300)
